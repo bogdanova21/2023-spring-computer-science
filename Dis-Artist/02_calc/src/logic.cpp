51bf5b01c6cf31d3874ab9185397a9c2bbdc6816
AlinaMaday <113342180+AlinaMaday@users.noreply.github.com>
2023-05-17 09:09:47 +0300

    Update logic.cpp

--- Dis-Artist/02_calc/src/logic.cpp
+++ Dis-Artist/02_calc/src/logic.cpp
@@ -29,3 +29,22 @@ namespace calc
         return output_ss.str();
     }
 }
+#include "logic.h"
+#include <sstream>
+
+namespace calc {
+    std::string Logic::process_math() {
+        if (expression.empty())
+            return "";
+        std::string input_ss;
+        input_ss.append(expression);
+        std::string output_ss;
+        double a = te_interp(input_ss.c_str(), 0);
+        output_ss = std::to_string(a);
+        output_ss.erase(output_ss.find_last_not_of('0') + 1, std::string::npos);
+        if (output_ss.back() == '.')
+            output_ss.replace(output_ss.find('.'), output_ss.find('.') + 1, "");
+
+        return output_ss;
+    }
+}
