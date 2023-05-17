1d525a66df27fc630765284d34df97dd4e095ad7
AlinaMaday <113342180+AlinaMaday@users.noreply.github.com>
2023-05-17 09:09:00 +0300

    Update logic.h

--- Dis-Artist/02_calc/src/logic.h
+++ Dis-Artist/02_calc/src/logic.h
@@ -15,3 +15,23 @@ namespace calc
 }
 
 #endif // LOGIC_HPP
+#ifndef LOGIC_HPP
+#define LOGIC_HPP
+
+#include <string>
+#include <cmath>
+#include "tinyexpr.h"
+
+
+namespace calc
+{
+
+    struct Logic
+    {
+        std::string process_math();
+        std::string expression;
+    };
+
+}
+
+#endif // LOGIC_HPP
